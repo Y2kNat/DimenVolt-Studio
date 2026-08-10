2fd2f16c17b59be4926c4954038234e27420126e
Isac O. <ornelasisac13@gmail.com>
2026-08-10 05:52:50 -0300

    Delete api/report directory

--- api/report/pdf.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-const { createPdfEndpoint } = require("../../src/reports/report-endpoint");
-const { generateReportPdf } = require("../../src/reports/pdfGenerator");
-
-module.exports = createPdfEndpoint(generateReportPdf, "DimenVolt-Relatorio");
-module.exports.config = { maxDuration: 60 };
--- api/report/quadro.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-const { createPdfEndpoint } = require("../../src/reports/report-endpoint");
-const { generateQuadroPdf } = require("../../src/reports/pdfGenerator");
-
-module.exports = createPdfEndpoint(generateQuadroPdf, "DimenVolt-Quadro");
-module.exports.config = { maxDuration: 60 };
