155eb87a1b2eb88a7cc5d4acd7007b0dcf9f5274
Isac O. <ornelasisac13@gmail.com>
2026-08-10 06:34:14 -0300

    Delete api/report directory

--- api/report/levantamento.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-const { createPdfEndpoint } = require("../../src/reports/report-endpoint");
-const { generateLevantamentoPdf } = require("../../src/reports/pdfGenerator");
-
-module.exports = createPdfEndpoint(generateLevantamentoPdf, "DimenVolt-Levantamento");
-module.exports.config = { maxDuration: 60 };
